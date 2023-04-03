3385df5a7f1fedad28f48408f4039f0dc5235fc5
Bijida Benjamin <50benbj@gmail.com>
2023-04-03 13:04:53 -0700

    Update Fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,12 +7,12 @@
 */
 size_t print_listint(const listint_t *h)
 {
-size_t num = 0;
-while (h)
+int i = 0;
+while(h != NULL)
 {
-printf("%d\n", h->n);
-num++;
+printf("%i\n", h->n);
 h = h->next;
+i++;
 }
-return (num);
+return (i);
 }
